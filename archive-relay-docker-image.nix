210e9e353a3ea45e03914b337fb42b4a97909f34
Martin Ondejka <ondejka99@gmail.com>
2024-10-14 14:54:59 +0200

    Add ssl certificate to archive relay image

--- archive-relay-docker-image.nix
+++ archive-relay-docker-image.nix
@@ -1,9 +1,10 @@
-{ bash, version, zeko_archive_relay, dockerTools }:
+{ bash, version, zeko_archive_relay, dockerTools, pkgs }:
 
 dockerTools.buildLayeredImage {
   name = "zeko-archive-relay";
   tag = "latest";
   contents = [ zeko_archive_relay bash ];
+  copyToRoot = [ pkgs.cacert ];
   config = {
     Entrypoint = [ "/bin/zeko-archive-relay" ];
   };
