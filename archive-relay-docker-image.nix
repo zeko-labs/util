b5424cbcbebe21df24c2bef462460bcaa22da19f
Martin Ondejka <ondejka99@gmail.com>
2024-10-14 15:19:12 +0200

    tmp

--- archive-relay-docker-image.nix
+++ archive-relay-docker-image.nix
@@ -1,11 +1,11 @@
-{ bash, version, zeko_archive_relay, dockerTools, pkgs }:
+{ bash, version, zeko_archive_relay, dockerTools, pkgs, coreutils }:
 
 dockerTools.buildLayeredImage {
   name = "zeko-archive-relay";
   tag = "latest";
-  contents = [ zeko_archive_relay bash ];
-  copyToRoot = [ pkgs.cacert ];
+  contents = [ zeko_archive_relay bash pkgs.cacert coreutils ];
   config = {
     Entrypoint = [ "/bin/zeko-archive-relay" ];
+    Env = [ "NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt" ];
   };
 }
